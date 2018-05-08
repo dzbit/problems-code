cded44770b178797259ea9ddb0dc2f124ece4b8f
dzbit <38690560+dzbit@users.noreply.github.com>
2018-05-08 13:51:20 -0500

    se corrige error

--- project-euler/1.MultiplesOf3And5.js
+++ project-euler/1.MultiplesOf3And5.js
@@ -1,16 +1,13 @@
 console.log("corriendo ..");
-
 //
 var cont = 0;
 var multiplos = function(rango, num1, num2){
   for(var i = 0; i <= rango; i++){
-    if((i % num1) === 0){ 
-      cont = cont + i;
-    }
-    if((i % num2) === 0){
+    if((i % num1) === 0 || (i % num2) === 0){ 
       cont = cont + i;
     }
   }
-  document.write(cont);
+  console.log(cont);
 }
 multiplos(1000, 3, 5);
+
