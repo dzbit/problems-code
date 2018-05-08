f7dcbf1eb9fe59a3a13e0d383182fb2559db472d
dzbit <38690560+dzbit@users.noreply.github.com>
2018-05-08 14:58:02 -0500

    modificacion algoritmo

--- project-euler/2.EvenFibonacciNumbers.js
+++ project-euler/2.EvenFibonacciNumbers.js
@@ -1,16 +1,14 @@
-var num1 = 0;
-var num2 = 1;
+var num1 = 1;
+var num2 = 2;
 var total;
 
 var fibonacci = function(rango){
   for(var i = 1; i <= rango/2; i++){
+    console.log(num1);
+    console.log(num2);
     num1 = num1 + num2;
-    num2 = num1 + num2;
-    total = num1;
-    console.log(total);
-    total = num2;
-    console.log(total);
+    num2 = num2 + num1;
   }
 };
 
-fibonacci(400);
+fibonacci(10);
